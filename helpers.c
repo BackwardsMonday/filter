1608b426adc2836ae3d7869b48458b873fc3298a
Daymon Wright <wrightdaymon@gmail.com>
2021-04-26 14:29:19 -0600

    initail commit

---
+++ helpers.c
@@ -0,0 +1,40 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            int newValue = round((pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3);
+
+            pixel.rgbtBlue = newValue;
+            pixel.rgbtRed = newValue;
+            pixel.rgbtGreen = newValue;
+
+            image[i][j] = pixel;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
