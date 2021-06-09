e00103f7c5751dffa5b58497d36990c7c54e16bb
Daymon Wright <wrightdaymon@gmail.com>
2021-06-09 12:35:48 -0600

    started edges function

--- helpers.c
+++ helpers.c
@@ -45,40 +45,40 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-typedef struct
-{
-    RGBTRIPLE rgbValue;
-    int integer;
-}rgbAndint;
 
-
-rgbAndint addSquare(int pixalsHight, int pixalsWidth, int height, int width, RGBTRIPLE image[height][width])
+RGBTRIPLE avregeSquare(int pixalsHight, int pixalsWidth, int height, int width, RGBTRIPLE image[height][width])
 {
-    rgbAndint newValue;
-    newValue.integer = 0;
-    newValue.rgbValue.rgbtBlue = 0;
-    newValue.rgbValue.rgbtGreen = 0;
-    newValue.rgbValue.rgbtRed = 0;
+    RGBTRIPLE newValue;
+    float values = 0;
+    newValue.rgbtBlue = 0;
+    newValue.rgbtGreen = 0;
+    newValue.rgbtRed = 0;
 
     for (int i = -1; i < 2; i++)
     {
-        for(int j  = -1; j < 2; j++)
+        for(int j = -1; j < 2; j++)
         {
             if ((i == -1 && pixalsHight == 0) || (i == 1 && pixalsHight == height-1) || (j == -1 && pixalsWidth == 0) || (j == 1 && pixalsWidth == width-1))
             {
                 continue;
             }
-            newValue.integer+= 1;
-            newValue.rgbValue.rgbtBlue+= image[pixalsHight+i][pixalsWidth+j].rgbtBlue;
-            newValue.rgbValue.rgbtRed+= image[pixalsHight+i][pixalsWidth+j].rgbtRed;
-            newValue.rgbValue.rgbtGreen+= image[pixalsHight+i][pixalsWidth+j].rgbtGreen;
-
+            else
+            {
+                values++;
+                newValue.rgbtBlue+= image[pixalsHight+i][pixalsWidth+j].rgbtBlue;
+                newValue.rgbtRed+= image[pixalsHight+i][pixalsWidth+j].rgbtRed;
+                newValue.rgbtGreen+= image[pixalsHight+i][pixalsWidth+j].rgbtGreen;
+            }
         }
     }
+    newValue.rgbtRed = round(newValue.rgbtRed/values);
+    newValue.rgbtBlue = round(newValue.rgbtBlue/values);
+    newValue.rgbtGreen = round(newValue.rgbtGreen/values);
+   
     return newValue;
 }
 void blur(int height, int width, RGBTRIPLE image[height][width])
-{
+{  
     RGBTRIPLE originalImage[height][width];
     for (int i = 0; i < height; i++)
     {
@@ -89,14 +89,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
     for (int i = 0; i < height; i++)
     {
-        for(int j  = 0; j < width; j++)
+        for(int j = 0; j < width; j++)
         {
-            rgbAndint newValue = addSquare(i, j, height, width, originalImage);
+            RGBTRIPLE newValue = avregeSquare(i, j, height, width, originalImage);
 
-            newValue.rgbValue.rgbtRed = newValue.rgbValue.rgbtRed/newValue.integer;
-            newValue.rgbValue.rgbtBlue = newValue.rgbValue.rgbtBlue/newValue.integer;
-            newValue.rgbValue.rgbtGreen = newValue.rgbValue.rgbtGreen/newValue.integer;
-            image[i][j] = newValue.rgbValue;
+            image[i][j] = newValue;
         }
     }
     return;
@@ -105,5 +102,52 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    int xKernel[3][3] =  {
+        {-1, 0, 1},
+        {-2, 0, 2},
+        {-1, 0, 1}
+    };
+    int yKernel[3][3] = {
+        {-1, -2, -1},
+        {0, 0, 0},
+        {1, 2, 1}
+    };
+    RGBTRIPLE originalImage[height][width];
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; i++)
+        {
+            originalImage[i][j] = image[i][j];
+        }
+    }
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixial = image[i][j];
+            RGBTRIPLE black;
+            black.rgbtBlue = 0;
+            black.rgbtGreen = 0;
+            black.rgbtRed = 0;
+            RGBTRIPLE Gx = black;
+            RGBTRIPLE Gy = black;
+
+            for(int k = 0; k < 2; k++)
+            {
+                Gx.rgbtBlue+= originalImage[k][j-1].rgbtBlue*xKernel[k][0];
+                Gx.rgbtGreen+= originalImage[k][j-1].rgbtGreen*xKernel[k][0];
+                Gx.rgbtRed+= originalImage[k][j-1].rgbtRed*xKernel[k][0];
+
+                Gx.rgbtBlue+= originalImage[k][j+1].rgbtBlue*xKernel[k][2];
+                Gx.rgbtGreen+= originalImage[k][j+1].rgbtGreen*xKernel[k][2];
+                Gx.rgbtRed+= originalImage[k][j+1].rgbtRed*xKernel[k][2];
+            }
+            for(int k = 0; k < 2; k++)
+            {
+                
+            }
+        }
+    }
     return;
 }
